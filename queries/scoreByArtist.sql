979eff1b725354daaad541513a824427c67b8193
jake <j3e8@users.noreply.github.com>
2018-09-27 10:33:23 -0700

    Update scoreByArtist.sql

--- queries/scoreByArtist.sql
+++ queries/scoreByArtist.sql
@@ -1,1 +1,5 @@
-select a.artistName, sum(101 - r.rank) as score from rankings as r inner join songs as s on r.songId=s.id inner join artists as a on s.artistId=a.id group by a.artistName order by score desc limit 20;
+select case when a.artistName = 'Elvis Presley With The Jordanaires' then 'Elvis Presley' else a.artistName end as artistName2, sum(101 - r.rank) as score
+from rankings as r
+inner join songs as s on r.songId=s.id
+inner join artists as a on s.artistId=a.id
+group by artistName2 order by score desc limit 100;
